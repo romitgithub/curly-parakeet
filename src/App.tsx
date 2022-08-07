47c2499a7534d54fa4f5a1f9f56a7c9a981a5904
Romit Choudhary <choudharyromit@gmail.com>
2022-08-07 12:22:24 +0530

    added lint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 function App() {
   return (
-    <div className="container p-2 mx-auto">
-      <h1 className="text-2xl font-semibold text-center">TODO App</h1>
+    <div className='container p-2 mx-auto'>
+      <h1 className='text-2xl font-semibold text-center'>TODO App</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,15 +1,14 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
 
 export const store = configureStore({
-  reducer: {
-  },
-});
+  reducer: {},
+})
 
-export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
   RootState,
   unknown,
   Action<string>
->;
+>
