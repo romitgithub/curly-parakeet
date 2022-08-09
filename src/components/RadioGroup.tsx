3fceef087479313433d7ceacf31054a73312f857
Romit Choudhary <choudharyromit@gmail.com>
2022-08-09 11:25:18 +0530

    added padding to radio items

--- src/components/RadioGroup.tsx
+++ src/components/RadioGroup.tsx
@@ -8,12 +8,13 @@ export default function RadioGroup({ options, defaultChecked, onChange }: RadioG
   return (
     <div className='mx-auto my-4 flex items-center w-fit'>
       {options.map((option) => (
-        <div key={option.id} className='flex items-center mx-4 cursor-pointer'>
+        <div key={option.id} className='flex items-center p-2 mx-4 cursor-pointer'>
           <input
             id={option.id}
             name='notification-method'
             type='radio'
             onChange={() => onChange(option.id)}
+            value={option.id}
             defaultChecked={option.id === defaultChecked}
             className='focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 cursor-pointer'
           />
